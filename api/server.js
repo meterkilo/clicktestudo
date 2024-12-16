fb97b226147c116873ff4357fff63e23c48162f0
matt <mattguo@terpmail.umd.edu>
2024-12-16 02:15:09 -0500

    test

--- api/server.js
+++ api/server.js
@@ -14,7 +14,10 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
 mongoose
-    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(
+        "mongodb+srv://matthew27888:123@cluster0.xrw8q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    )
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.error("MongoDB connection error:", err));
 
