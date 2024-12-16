ea56697063a6c0930d162ee1b65c5adcf710ddeb
matt <mattguo@terpmail.umd.edu>
2024-12-16 02:17:34 -0500

    fg

--- api/server.js
+++ api/server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
 
 mongoose
