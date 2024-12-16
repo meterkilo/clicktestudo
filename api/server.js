70f2370f1b8feb60711ec0f6b04d6b1a7c193a9e
matt <mattguo@terpmail.umd.edu>
2024-12-16 02:26:02 -0500

    fixed pathing

--- api/server.js
+++ api/server.js
@@ -7,10 +7,9 @@ const axios = require("axios");
 const User = require("../models/User");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
 
